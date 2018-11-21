2c51644af85752905633e3676708a3c516bafdb7
Kevin Simpson <relasine@gmail.com>
2018-11-20 20:07:26 -0700

    preloading

--- src/components/Preload.js
+++ src/components/Preload.js
@@ -1,18 +1,28 @@
 import React from "react";
+import darkworld1 from "../css/images/darkworld1.png";
+import darkworld2 from "../css/images/darkworld2.png";
+import deck1 from "../css/images/deck1.png";
+import deck2 from "../css/images/deck2.png";
+import grass from "../css/images/grass.png";
+import house from "../css/images/house.png";
+import itemshop from "../css/images/itemshop.png";
+import sidewalk from "../css/images/sidewalk.png";
+import socialBoard from "../css/images/social-board.png";
+import tavern from "../css/images/tavern.png";
 
 const Preload = () => {
   return (
     <div className="preload">
-      <img src="../css/images/darkworld1.png" alt="darkworld1" />
-      <img src="../css/images/darkworld2.png" alt="darkworld2" />
-      <img src="../css/images/deck1.png" alt="deck1" />
-      <img src="../css/images/deck2.png" alt="deck2" />
-      <img src="../css/images/grass.png" alt="grass" />
-      <img src="../css/images/house.png" alt="house" />
-      <img src="../css/images/itemshop.png" alt="itemshop" />
-      <img src="../css/images/sidewalk.png" alt="sidewalk" />
-      <img src="../css/images/social-board.png" alt="social-board" />
-      <img src="../css/images/tavern.png" alt="tavern" />
+      <img src={darkworld1} alt="darkworld1" />
+      <img src={darkworld2} alt="darkworld2" />
+      <img src={deck1} alt="deck1" />
+      <img src={deck2} alt="deck2" />
+      <img src={grass} alt="grass" />
+      <img src={house} alt="house" />
+      <img src={itemshop} alt="itemshop" />
+      <img src={sidewalk} alt="sidewalk" />
+      <img src={socialBoard} alt="social-board" />
+      <img src={tavern} alt="tavern" />
     </div>
   );
 };
